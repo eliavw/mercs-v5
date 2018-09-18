cd3bf20848ccb88ef08b80fc224d04b1b890d76a
elia <Elia.vw@gmail.com>
2018-09-18 15:44:34 +0200

    More classification integration test

--- src/tests/integration/test_basic_classification.py
+++ src/tests/integration/test_basic_classification.py
@@ -19,16 +19,16 @@ from mercs.utils import *
 import datasets as datasets
 
 
-def test_basic_classification():
+def setup_classification():
     train, test = datasets.load_nursery()
     model = MERCS()
 
     ind_parameters = {'ind_type':           'RF',
                       'ind_n_estimators':   10}
 
-    sel_parameters = {'sel_type':       'Base',
-                      'sel_its':        4,
-                      'sel_param':      2}
+    sel_parameters = {'sel_type':           'Base',
+                      'sel_its':            4,
+                      'sel_param':          2}
 
     model.fit(train, **ind_parameters, **sel_parameters)
 
@@ -36,6 +36,11 @@ def test_basic_classification():
 
     target_boolean = np.array(code) == 1
     y_true = test[test.columns.values[target_boolean]].values
+    return train, test, code, model, y_true
+
+
+def test_MI_classification():
+    train, test, code, model, y_true = setup_classification()
 
     pred_parameters = {'pred_type':     'MI',
                        'pred_param':    0.95,
@@ -51,29 +56,12 @@ def test_basic_classification():
     assert 0 <= obs <= 1
 
 
-def test_avd_classification():
-    train, test = datasets.load_nursery()
-    model = MERCS()
+def test_MA_classification():
+    train, test, code, model, y_true = setup_classification()
 
-    ind_parameters = {'ind_type':           'RF',
-                      'ind_n_estimators':   10,
-                      'ind_max_depth':      4}
-
-    sel_parameters = {'sel_type':       'Base',
-                      'sel_its':        4,
-                      'sel_param':      2}
-
-    model.fit(train, **ind_parameters, **sel_parameters)
-
-    code = [0, 0, 0, 0, 0, 0, 0, 0, 1]
-
-    target_boolean = np.array(code) == 1
-    y_true = test[test.columns.values[target_boolean]].values
-
-    # Test 01
-    pred_parameters = {'pred_type':         'MA',
-                       'pred_param':        0.95,
-                       'pred_its':          0.1}
+    pred_parameters = {'pred_type':     'MA',
+                       'pred_param':    0.95,
+                       'pred_its':      0.1}
 
     y_pred = model.predict(test,
                            **pred_parameters,
@@ -84,10 +72,46 @@ def test_avd_classification():
     assert isinstance(obs, (int, float))
     assert 0 <= obs <= 1
 
-    # Test 02
-    pred_parameters = {'pred_type':         'MAFI',
-                       'pred_param':        0.95,
-                       'pred_its':          0.1}
+
+def test_MAFI_classification():
+    train, test, code, model, y_true = setup_classification()
+
+    pred_parameters = {'pred_type':     'MAFI',
+                       'pred_param':    0.95,
+                       'pred_its':      0.1}
+
+    y_pred = model.predict(test,
+                           **pred_parameters,
+                           qry_code=code)
+
+    obs = f1_score(y_true, y_pred, average='macro')
+
+    assert isinstance(obs, (int, float))
+    assert 0 <= obs <= 1
+
+
+def test_IT_classification():
+    train, test, code, model, y_true = setup_classification()
+
+    pred_parameters = {'pred_type':     'IT',
+                       'pred_param':    0.1,
+                       'pred_its':      8}
+
+    y_pred = model.predict(test,
+                           **pred_parameters,
+                           qry_code=code)
+
+    obs = f1_score(y_true, y_pred, average='macro')
+
+    assert isinstance(obs, (int, float))
+    assert 0 <= obs <= 1
+
+def test_RW_classification():
+    train, test, code, model, y_true = setup_classification()
+
+    pred_parameters = {'pred_type':     'RW',
+                       'pred_param':    2,
+                       'pred_its':      16}
 
     y_pred = model.predict(test,
                            **pred_parameters,
