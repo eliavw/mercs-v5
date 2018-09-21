eb9fce1ce8d791206a36e91029198be745929183
elia <Elia.vw@gmail.com>
2018-09-21 11:31:36 +0200

    Better datasets handling

--- src/datasets/datasets.py
+++ src/datasets/datasets.py
@@ -10,22 +10,33 @@ for dname in {'src'}:
     sys.path.append(os.path.join(root_directory, dname))
 from mercs.utils.debug import debug_print
 
-VERBOSITY = 1
+VERBOSITY = 0
+
 
 # Actual method
 def load_example_dataset(dataset_name, specifier=None, extension=None):
     """
-    Load example dataset (.data_csv) with given name.
+    Load example dataset (.csv) with given name.
 
-    :param dataset_name:    Name of example dataset
-    :param specifier:       {'Test', 'Train}
-    :return:                Pandas Dataframe with the dataset
+    Parameters
+    ----------
+    dataset_name: string
+        Name of the example dataset
+    specifier: {string, NoneType}
+        x in  {'Train', 'Test', None}
+    extension: string
+        Extension of the filename
+
+    Returns
+    -------
+    df: pd.DataFrame
+        DataFrame of the example dataset
+
     """
     root_dir = dirname(dirname(dirname(__file__)))
     data_dir = os.path.join(root_dir, 'resc', 'data')
 
     extension = '.csv' if extension is None else '.' + extension
-
     appendix = '' if specifier is None else '_' + specifier
 
     fname = os.path.join(data_dir, dataset_name + appendix + extension)
@@ -37,8 +48,9 @@ def load_example_dataset(dataset_name, specifier=None, extension=None):
     assert isinstance(df, pd.DataFrame)
     return df
 
-# Custom shortcuts
-def load_fertility():
+
+# Shortcuts
+def load_fertility(full=False):
     """
     Load the fertility dataset.
 
@@ -47,104 +59,83 @@ def load_fertility():
     :return:
     """
 
-    train = load_example_dataset('fertility', 'train')
-    test = load_example_dataset('fertility', 'test')
+    if full:
+        return load_example_dataset('fertility')
+    else:
+        train = load_example_dataset('fertility', 'train')
+        test = load_example_dataset('fertility', 'test')
 
-    return train, test
+        return train, test
 
 
-def load_jester():
+def load_jester(full=False):
     """
     Load the jester dataset
 
+    Nominal dataset
+
     :return:
     """
 
-    train = load_example_dataset('jester', 'train')
-    test = load_example_dataset('jester', 'test')
+    if full:
+        return load_example_dataset('jester')
+    else:
+        train = load_example_dataset('jester', 'train')
+        test = load_example_dataset('jester', 'test')
 
-    return train, test
+        return train, test
 
 
-def load_netflix():
+def load_netflix(full=False):
     """
     Load the netflix dataset
 
+    Nominal dataset
+
     :return:
     """
 
-    train = load_example_dataset('netflix', 'train')
-    test = load_example_dataset('netflix', 'test')
+    if full:
+        return load_example_dataset('netflix')
+    else:
+        train = load_example_dataset('netflix', 'train')
+        test = load_example_dataset('netflix', 'test')
 
-    return train, test
+        return train, test
 
 
-def load_nursery():
+def load_nursery(full=False):
     """
     Load the nursery dataset
 
+    Nominal dataset
+
     :return:
     """
 
-    train = load_example_dataset('nursery', 'train')
-    test = load_example_dataset('nursery', 'test')
+    if full:
+        return load_example_dataset('nursery')
+    else:
+        train = load_example_dataset('nursery', 'train')
+        test = load_example_dataset('nursery', 'test')
 
-    return train, test
+        return train, test
 
 
-def load_slump():
+def load_slump(full=False):
     """
     Load the slump dataset
 
+    Numeric dataset
+
     :return:
     """
 
-    train = load_example_dataset('slump', 'train')
-    test = load_example_dataset('slump', 'test')
+    if full:
+        return load_example_dataset('slump')
+    else:
+        train = load_example_dataset('slump', 'train')
+        test = load_example_dataset('slump', 'test')
 
-    return train, test
+        return train, test
 
-
-def load_fertility_full():
-    """
-    Load the full fertility.data_csv file.
-
-    :return:    Pandas DataFrame of this dataset.
-    """
-    return load_example_dataset('fertility')
-
-
-def load_jester_full():
-    """
-    Load the full jester.data_csv file.
-
-    :return:    Pandas DataFrame of this dataset.
-    """
-    return load_example_dataset('jester')
-
-
-def load_netflix_full():
-    """
-    Load the full netflix.data_csv file.
-
-    :return:    Pandas DataFrame of this dataset.
-    """
-    return load_example_dataset('netflix')
-
-
-def load_nursery_full():
-    """
-    Load the full nursery.data_csv file.
-
-    :return:    Pandas DataFrame of this dataset.
-    """
-    return load_example_dataset('nursery')
-
-
-def load_slump_full():
-    """
-    Load the full slump.data_csv file.
-
-    :return:    Pandas DataFrame of this dataset.
-    """
-    return load_example_dataset('slump')
