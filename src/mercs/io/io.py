0e1e2609df77a9a2f81c0d1ec947fe0ee64ff05d
elia <Elia.vw@gmail.com>
2018-09-24 16:01:17 +0200

    Updated io

--- src/mercs/io/io.py
+++ src/mercs/io/io.py
@@ -3,6 +3,25 @@ import pandas as pd
 
 def save_output_data(Y, q_targ, filename):
     """
+    Write predictions to file
+
+    Parameters
+    ----------
+    Y: np.ndarray, shape (nb_samples, nb_targ_attributes)
+        Numpy array of results
+    q_targ: list, shape (nb_targ,)
+        List of integers which are the indices of the attributes that
+        compose the target set of attributes of the current prediction.
+
+        These are used to compose a default header for the output csv file.
+    filename: str
+        Filename where the results should be saved
+
+    Returns
+    -------
+
+    """
+    """
     Given filename + Numpy array of predictions, write predictions to file.
 
     :param Y:               Numpy array of results
@@ -12,9 +31,27 @@ def save_output_data(Y, q_targ, filename):
     """
 
     # Building header (names of target attributes)
-    header = ["feature {}".format(i) for i in q_targ]
+    header = _compose_default_header(q_targ)
 
     df = pd.DataFrame(Y)
     df.to_csv(filename, header=header, index=False)
 
     return
+
+
+def _compose_default_header(attributes):
+    """
+    Compose default header for the csv file.
+
+    Parameters
+    ----------
+    attributes: list, shape (nb_targ,)
+        List of integers which are the integers of the attributes that
+        should be mentioned in the header.
+
+    Returns
+    -------
+    """
+
+    header = ["feature {}".format(i) for i in attributes]
+    return header
