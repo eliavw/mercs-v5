70e61c6ff867a490b1fe390df64aa1fca7d1dda0
eliavw <elia.vw@gmail.com>
2018-09-26 16:28:19 +0200

    Adding initial docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,8 +43,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'numpydoc',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
